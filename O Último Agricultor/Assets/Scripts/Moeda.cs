db48983d49cf9e6ca587ac7676d9455f39dcac57
DallaProRacing <103423@aluno.uricer.edu.br>
2026-02-28 14:39:01 -0300

    Adicionando o som da meoda sendo coletada

--- O Último Agricultor/Assets/Scripts/Moeda.cs
+++ O Último Agricultor/Assets/Scripts/Moeda.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class Moeda : MonoBehaviour
 {
+
+    public AudioSource somMoeda;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            GetComponent<SpriteRenderer>().enabled = false;
             other.GetComponent<CodigoJogador>().AumentarQuantidadeMoedas();
+            somMoeda.Play();
             Debug.Log("Moeda colidio com o jogador.");
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, 0.5f);
         }
     }
 
